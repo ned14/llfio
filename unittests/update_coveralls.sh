dc55e5bb86afded717253e6c4f07b0989ea3ade6
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <noone@spamtrap>
2013-06-05 18:58:09 -0400

    Print both sides of JSON transaction.

--- unittests/update_coveralls.sh
+++ unittests/update_coveralls.sh
@@ -27,9 +27,12 @@ do
 EOF
 done
 
+cat coverage.json
 mv coverage.json coverage.json.tmp
 cat >coverage.json <(head -n -1 coverage.json.tmp) <(echo -e "    }\n  ]\n}")
 rm *.gcov coverage.json.tmp
 
+cat coverage.json
+echo
 curl -F json_file=@coverage.json https://coveralls.io/api/v1/jobs
 cat coverage.json
