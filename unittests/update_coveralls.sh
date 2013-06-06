251c7d37200894e6283508185040246f1a263a93
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2013-06-05 21:14:50 -0400

    Print what sed sees

--- unittests/update_coveralls.sh
+++ unittests/update_coveralls.sh
@@ -21,7 +21,7 @@ do
   cat >>coverage.json <<EOF
     {
       "name": "$(echo ${file} | sed -re 's%#%\/%g; s%.gcov$%%')",
-      "source": "deleted",
+      "source": "$(tail -n +3 ${file} | cut -d ':' -f 3-')",
       "coverage": [$(tail -n +3 ${file} | cut -d ':' -f 1 | sed -re 's%^ +%%g; s%-%null%g; s%^[#=]+$%0%;' | tr $'\n' ',' | sed -re 's%,$%%')]
     },
 EOF
