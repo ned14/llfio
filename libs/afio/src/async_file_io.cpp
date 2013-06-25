f31528e1dd8deef5f73bf4862162bfe41971fa23
Paul Kirth <pk1574@gmail.com>
2013-06-25 01:57:51 -0700

    Edits to Visual Studio Projects and a fix to some macros in libs/afio/src/async_file_io.cpp
    This commit focused on getting VS2012 projects up to date.
    
    An issue arose with BOOST_AFIO_POSIX_STAT macro under windows
    There are "_stat64" and "_wstat64" on windows, while only one "stat" under
    POSIX, so to fix the issue I changed the macros to reflect this as best as
    possible.
    
    This solution leaves something to be desired, and should be fixed with a
    better name when possible.
    
    Compiles under MSVC 2012 w/ November2012 CTP compiler

--- libs/afio/src/async_file_io.cpp
+++ libs/afio/src/async_file_io.cpp
@@ -32,8 +32,8 @@ File Created: Mar 2013
 #include <direct.h>
 #define BOOST_AFIO_POSIX_MKDIR(path, mode) _wmkdir(path)
 #define BOOST_AFIO_POSIX_RMDIR _wrmdir
-#define BOOST_AFIO_POSIX_STAT _wstat64
-#define BOOST_AFIO_POSIX_STAT _stat64
+#define BOOST_AFIO_POSIX_STAT _wstat64      //Part of the windows work around
+#define BOOST_AFIO_STAT _stat64           // BOOST_POSIX_STAT caused an error under windows because it is defined twice, need to fix this for all systems
 #define BOOST_AFIO_POSIX_S_ISREG(m) ((m) & _S_IFREG)
 #define BOOST_AFIO_POSIX_S_ISDIR(m) ((m) & _S_IFDIR)
 #define BOOST_AFIO_POSIX_OPEN _wopen
@@ -46,7 +46,8 @@ File Created: Mar 2013
 #include <limits.h>
 #define BOOST_AFIO_POSIX_MKDIR mkdir
 #define BOOST_AFIO_POSIX_RMDIR ::rmdir
-#define BOOST_AFIO_POSIX_STAT stat
+#define BOOST_AFIO_STAT stat                // work around for windows compatability
+#define BOOST_AFIO_POSIX_STAT stat          // Original
 #define BOOST_AFIO_POSIX_OPEN open
 #define BOOST_AFIO_POSIX_CLOSE ::close
 #define BOOST_AFIO_POSIX_UNLINK unlink
@@ -54,7 +55,6 @@ File Created: Mar 2013
 #define BOOST_AFIO_POSIX_FTRUNCATE ftruncate
 #define BOOST_AFIO_POSIX_S_ISREG S_ISREG
 #define BOOST_AFIO_POSIX_S_ISDIR S_ISDIR
-#define BOOST_AFIO_POSIX_STAT stat
 #endif
 
 // libstdc++ doesn't come with std::lock_guard
@@ -1040,7 +1040,7 @@ namespace detail {
 				req.flags=req.flags&~(file_flags::Create|file_flags::CreateOnlyIfNotExist);
 			}
 
-			struct BOOST_AFIO_POSIX_STAT s={0};
+			struct BOOST_AFIO_STAT s={0};
 			ret=BOOST_AFIO_POSIX_STAT(req.path.c_str(), &s);
 			if(0==ret && !BOOST_AFIO_POSIX_S_ISDIR(s.st_mode))
 				throw std::runtime_error("Not a directory");
