d64da7e2aec4a64a3ba4a667f73fb9c97950944d
Paul Kirth <pk1574@gmail.com>
2013-07-18 12:16:16 -0700

    Begin creating test_all to run all the unit tests together

---
+++ libs/afio/test/test_all.cpp
@@ -0,0 +1,12 @@
+#define BOOST_AFIO_STANDALONE_TESTS
+
+#include "test_functions.h"
+
+
+#include "async_io_threadpool_test.cpp"
+#include "async_io_works_1_prime_test.cpp"
+#include "async_io_works_1_test.cpp"
+#include "async_io_works_64_test.cpp"
+
+
+#include "async_io_barrier_test.cpp"
