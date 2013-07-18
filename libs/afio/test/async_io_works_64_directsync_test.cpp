1596a9d43845c21dcd424223e00db401efb5f7db
Paul Kirth <pk1574@gmail.com>
2013-07-18 11:42:13 -0700

    Added the last of the unit test files from main.cpp
    TODO:
    1. make a test_all that includes all the unit tests
    2. make changes to the test jamfile to use valgrind
    3. make a bjam rule to pass via command line args to bjam to build either valgrind or regular unit tests
    4. make a bjam rule to select all tests be run together or individually
    5. remove main.cpp after this works

---
+++ libs/afio/test/async_io_works_64_directsync_test.cpp
@@ -0,0 +1,9 @@
+#include "test_functions.h"
+
+BOOST_AUTO_TEST_CASE(async_io_works_64_directsync)
+{
+    BOOST_TEST_MESSAGE("Tests that the direct synchronous async i/o implementation works");
+    auto dispatcher = boost::afio::async_file_io_dispatcher(boost::afio::process_threadpool(), boost::afio::file_flags::OSDirect | boost::afio::file_flags::OSSync);
+    std::cout << "\n\n1000 file opens, writes 64Kb, closes, and deletes with direct synchronous i/o:\n";
+    _1000_open_write_close_deletes(dispatcher, 65536);
+}
