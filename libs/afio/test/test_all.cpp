46740b0fd9e2e20d52d4d06e40a01ba5cc15c915
Paul Kirth <pk1574@gmail.com>
2013-07-19 00:01:14 -0700

    Changes to Jamfiles and test_all.cpp to build properly
    I had improperly linked one of the tests, so it failed
    
    We should consider looking into a way to consolidate the test results at the end of the test run, rather that having pass/fail dispersed through the entire output. Maybe there is a facility in Boost.Build that can do that.

--- libs/afio/test/test_all.cpp
+++ libs/afio/test/test_all.cpp
@@ -18,12 +18,11 @@ BOOST_AUTO_TEST_SUITE(all)
 #include "async_io_works_64_directsync_test.cpp"
 #include "async_io_barrier_test.cpp"
 #include "async_io_errors_test.cpp"
-#include "async_io_errors_test.cpp"
 #include "async_io_torture_test.cpp"
 #include "async_io_torture_sync_test.cpp"
 #include "async_io_torture_autoflush_test.cpp"
 #include "async_io_torture_direct_test.cpp"
-#include "async_io_torture_directsync_test.cpp"
+#include "async_io_torture_direct_sync_test.cpp"
 #include "async_io_sync_test.cpp"
 BOOST_AUTO_TEST_SUITE_END()
 
