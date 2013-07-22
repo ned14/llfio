cb6f1029d479423f6cce388054944c8b99fad654
Paul Kirth <pk1574@gmail.com>
2013-07-22 15:51:33 -0700

    Began auto-generating test_all.cpp, and globing the unit tests in the jamfile

--- libs/afio/test/test_all.cpp
+++ libs/afio/test/test_all.cpp
@@ -5,6 +5,9 @@
 #include "test_functions.hpp"
 
 BOOST_AUTO_TEST_SUITE(all)
+        
+#include "test_cpps.txt"        
+/*     
 #include "async_io_threadpool_test.cpp"
 #include "async_io_works_1_prime_test.cpp"
 #include "async_io_works_1_test.cpp"
@@ -24,7 +27,7 @@ BOOST_AUTO_TEST_SUITE(all)
 #include "async_io_torture_autoflush_test.cpp"
 #include "async_io_torture_direct_test.cpp"
 #include "async_io_torture_direct_sync_test.cpp"
-#include "async_io_sync_test.cpp"
+#include "async_io_sync_test.cpp" // */
 BOOST_AUTO_TEST_SUITE_END()
 
 
