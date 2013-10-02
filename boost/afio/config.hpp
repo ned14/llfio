2ba4559862dc3c8f8ad263cae36f87d4240b5938
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere.com>
2013-10-02 19:45:06 -0400

    Moved source files to header impl

--- boost/afio/config.hpp
+++ boost/afio/config.hpp
@@ -21,17 +21,6 @@
 #include <boost/system/api_config.hpp>  // for BOOST_POSIX_API or BOOST_WINDOWS_API
 #include <boost/detail/workaround.hpp> 
 
-//Why do they use BOOST_CLANG and not BOOST_MSVC ???
-#if (defined(_MSC_VER) && (_MSC_VER >= 1020)) || defined(__GNUC__) || defined(BOOST_CLANG) || defined(BOOST_INTEL) || defined(__COMO__) || defined(__DMC__)
-#define BOOST_AFIO_HAS_PRAGMA_ONCE
-#endif
-
-
-//Does this even help inside of an include guard?? 
-#ifdef BOOST_AFIO_HAS_PRAGMA_ONCE
-#pragma once
-#endif
-
 ///////////////////////////////////////////////////////////////////////////////
 //  Set up dll import/export options
 #if (defined(BOOST_AFIO_DYN_LINK) || defined(BOOST_ALL_DYN_LINK)) && \
