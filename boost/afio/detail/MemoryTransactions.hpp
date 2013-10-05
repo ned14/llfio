e093796d5e21734ea5eadb6d67bf0ed4d71b037b
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere.com>
2013-10-04 21:54:32 -0400

    Tried removing const.

--- boost/afio/detail/MemoryTransactions.hpp
+++ boost/afio/detail/MemoryTransactions.hpp
@@ -181,7 +181,7 @@ namespace boost
                     asm volatile(".byte 0x0f,0x01,0xd5" ::: "memory");
                 }
 
-                static inline void _xabort(const unsigned int status) BOOST_NOEXCEPT_OR_NOTHROW
+                static inline void _xabort(unsigned int status) BOOST_NOEXCEPT_OR_NOTHROW
                 {
                     asm volatile(".byte 0xc6,0xf8,%P0" :: "i" (status) : "memory");
                 }
