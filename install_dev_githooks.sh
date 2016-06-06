dc258685f712aeb6784a84425d6391154ffd4a07
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-06-06 22:33:14 +0100

    fix install_dev_githooks.sh

--- install_dev_githooks.sh
+++ install_dev_githooks.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo Installing post-receive hook which recursively forces all
 echo submodules to latest commit after every git pull
-cat <<EOT >> .git/hooks/post-receive
+cat <<EOT >> .git/hooks/post-merge
 #!/bin/sh
 git submodule foreach --recursive 'branch=\$(git config -f \$toplevel/.gitmodules submodule.\$name.branch); [ \"\$branch\" = \"\" ] && branch=master; git checkout \$branch; git merge origin/$branch;'
 EOT
-chmod +x .git/hooks/post-receive
+chmod +x .git/hooks/post-merge
