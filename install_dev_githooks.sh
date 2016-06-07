69adff7509d985ea608afbcb7050328185214c97
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-06-07 21:19:56 +0100

    Fix yet another bug in the githooks

--- install_dev_githooks.sh
+++ install_dev_githooks.sh
@@ -3,7 +3,7 @@ echo Installing post-merge hook which recursively forces all
 echo submodules to latest commit after every git pull
 cat <<EOT >> .git/hooks/post-merge
 #!/bin/sh
-git submodule foreach --recursive 'branch=\$(git config -f \$toplevel/.gitmodules submodule.\$name.branch); [ \"\$branch\" = \"\" ] && branch=master; git checkout \$branch; git merge origin/$branch;'
+git submodule foreach --recursive 'branch=\$(git config -f \$toplevel/.gitmodules submodule.\$name.branch); [ \"\$branch\" = \"\" ] && branch=master; git checkout \$branch; git merge origin/\$branch;'
 EOT
 chmod +x .git/hooks/post-merge
 echo Installing custom 'git pull-all' command for recursively
