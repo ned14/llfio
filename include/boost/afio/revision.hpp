d1026da887ba0e670b032c18a20bef4898c650b6
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-07-03 13:23:08 +0100

    cmake version is now parsed from version.hpp

---
+++ include/boost/afio/revision.hpp
@@ -0,0 +1,3 @@
+// Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
+#define BOOST_AFIO_PREVIOUS_COMMIT_REF  x
+#define BOOST_AFIO_PREVIOUS_COMMIT_DATE "x"
