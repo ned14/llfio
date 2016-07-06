8f2b0db372fdf0b43592120197dc6d9192b20e45
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-07-06 07:12:06 +0100

    revision.hpp now updated with latest git commit

--- include/boost/afio/revision.hpp
+++ include/boost/afio/revision.hpp
@@ -1,3 +1,3 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define BOOST_AFIO_PREVIOUS_COMMIT_REF  x
-#define BOOST_AFIO_PREVIOUS_COMMIT_DATE "x"
+#define BOOST_AFIO_PREVIOUS_COMMIT_REF  5496ce656f09a15ad280ef10af10202508648259
+#define BOOST_AFIO_PREVIOUS_COMMIT_DATE "2016-07-05T09:14:46"
