e87a0e29976d5bb7fde6df93951a87a23323ce50
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-07-08 09:33:30 +0100

    Now actually generating proper precompiled headers thanks to some undocumented cmake support for precompiled headers. Woohoo.

--- include/boost/afio/revision.hpp
+++ include/boost/afio/revision.hpp
@@ -1,3 +1,3 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define BOOST_AFIO_PREVIOUS_COMMIT_REF  8f2b0db372fdf0b43592120197dc6d9192b20e45
-#define BOOST_AFIO_PREVIOUS_COMMIT_DATE "2016-07-06 06:12:06 +00:00"
+#define BOOST_AFIO_PREVIOUS_COMMIT_REF  ab81a76cc6fbfb2ff551bd6fed9ecd6e26047238
+#define BOOST_AFIO_PREVIOUS_COMMIT_DATE "2016-07-06 08:34:15 +00:00"
