b1d25e3021caa6085ad14e51f4704ef2966ca587
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-07-09 14:52:20 +0100

    Fixed the invalid preprocessor in afio.hpp

--- include/boost/afio/revision.hpp
+++ include/boost/afio/revision.hpp
@@ -1,3 +1,3 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define BOOST_AFIO_PREVIOUS_COMMIT_REF  ab81a76cc6fbfb2ff551bd6fed9ecd6e26047238
-#define BOOST_AFIO_PREVIOUS_COMMIT_DATE "2016-07-06 08:34:15 +00:00"
+#define BOOST_AFIO_PREVIOUS_COMMIT_REF  cfd6b087cc84fea7793905f5ff4b1e1576bc0486
+#define BOOST_AFIO_PREVIOUS_COMMIT_DATE "2016-07-09 13:50:41 +00:00"
