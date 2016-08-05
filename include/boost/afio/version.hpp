3f474fa12835b812ae039a5341e07d76818a7886
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2016-08-05 09:38:39 +0100

    Update docs

--- include/boost/afio/version.hpp
+++ include/boost/afio/version.hpp
@@ -1,3 +1,4 @@
+//! \file version.hpp Controls the version of AFIO for cmake, shared library and C++ namespace mangling
 #undef BOOST_AFIO_VERSION_MAJOR
 #undef BOOST_AFIO_VERSION_MINOR
 #undef BOOST_AFIO_VERSION_PATCH
@@ -7,23 +8,25 @@
 #undef BOOST_AFIO_HEADERS_VERSION
 #undef BOOST_AFIO_NAMESPACE_VERSION
 
-#define BOOST_AFIO_VERSION_MAJOR    2  // Major version for cmake and DLL version stamping
-#define BOOST_AFIO_VERSION_MINOR    0  // Minor version for cmake and DLL version stamping
-#define BOOST_AFIO_VERSION_PATCH    0  // Patch version for cmake and DLL version stamping
-#define BOOST_AFIO_VERSION_REVISION 0  // Revision version for cmake and DLL version stamping
+//! \brief Major version for cmake and DLL version stamping
+#define BOOST_AFIO_VERSION_MAJOR    2
+//! \brief Minor version for cmake and DLL version stamping
+#define BOOST_AFIO_VERSION_MINOR    0
+//! \brief Patch version for cmake and DLL version stamping
+#define BOOST_AFIO_VERSION_PATCH    0
+//! \brief Revision version for cmake and DLL version stamping
+#define BOOST_AFIO_VERSION_REVISION 0
 
-// Defined between stable releases of AFIO
+//! \brief Defined between stable releases of AFIO. It means the inline namespace
+//! will be permuted per-commit to ensure ABI uniqueness.
 #define BOOST_AFIO_UNSTABLE_VERSION
 
 #define BOOST_AFIO_VERSION_GLUE2(a, b, c) a ## b ## c
 #define BOOST_AFIO_VERSION_GLUE(a, b, c)  BOOST_AFIO_VERSION_GLUE2(a, b, c)
-// The namespace boost::afio::v ## BOOST_AFIO_NAMESPACE_VERSION
 #define BOOST_AFIO_NAMESPACE_VERSION   BOOST_AFIO_VERSION_GLUE(BOOST_AFIO_VERSION_MAJOR, _, BOOST_AFIO_VERSION_MINOR)
 
 #if defined(_MSC_VER) && !defined(__clang)
-// The path for the headers v ## BOOST_AFIO_HEADERS_VERSION
 #define BOOST_AFIO_HEADERS_VERSION     BOOST_AFIO_VERSION_GLUE(BOOST_AFIO_VERSION_MAJOR, ., BOOST_AFIO_VERSION_MINOR)
 #else
-// The path for the headers v ## BOOST_AFIO_HEADERS_VERSION
 #define BOOST_AFIO_HEADERS_VERSION     BOOST_AFIO_VERSION_MAJOR.BOOST_AFIO_VERSION_MINOR
 #endif
