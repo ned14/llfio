7af6c4e0826b26e84ed10a61fc96a59841c95f00
Jenkins nedprod CI <foo@nowhere>
2016-09-12 07:25:16 +0000

    Travis CI updates documentation

--- search/functions_5f.js
+++ search/functions_5f.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['_5fhash_5fentities',['_hash_entities',['../classboost_1_1afio_1_1v2__xxx_1_1algorithm_1_1shared__fs__mutex_1_1memory__map.html#ae2d575461edb9d27c701a8e60671069f',1,'boost::afio::v2_xxx::algorithm::shared_fs_mutex::memory_map']]],
   ['_5flock',['_lock',['../classboost_1_1afio_1_1v2__xxx_1_1algorithm_1_1shared__fs__mutex_1_1atomic__append.html#a4c16831d072f5e2ec9a33532b6764616',1,'boost::afio::v2_xxx::algorithm::shared_fs_mutex::atomic_append']]]
 ];
