19b252b8388303077accbe9bc2598977faec14d7
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2017-05-03 00:53:13 +0100

    Relicenced under Apache 2.0 + Boost licence

--- test/test_kernel_decl.hpp
+++ test/test_kernel_decl.hpp
@@ -1,6 +1,25 @@
 /* Allows the test kernel to be compiled standalone
-(C) 2016 Niall Douglas http://www.nedproductions.biz/
+(C) 2016-2017 Niall Douglas <http://www.nedproductions.biz/> (6 commits)
 Created: May 2016
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License in the accompanying file
+Licence.txt or at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+Distributed under the Boost Software License, Version 1.0.
+    (See accompanying file Licence.txt or copy at
+          http://www.boost.org/LICENSE_1_0.txt)
 */
 
 #ifndef BOOST_AFIO_TEST_KERNEL_DECL_HPP
--- test/tests/file_handle_create_close/kernel_async_file_handle.cpp.hpp
+++ test/tests/file_handle_create_close/kernel_async_file_handle.cpp.hpp
@@ -1,6 +1,25 @@
 /* Test kernel for file_handle create and close
-(C) 2016 Niall Douglas http://www.nedprod.com/
+(C) 2016-2017 Niall Douglas <http://www.nedproductions.biz/> (4 commits)
 File Created: May 2016
+
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License in the accompanying file
+Licence.txt or at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+Distributed under the Boost Software License, Version 1.0.
+    (See accompanying file Licence.txt or copy at
+          http://www.boost.org/LICENSE_1_0.txt)
 */
 
 #include "../../test_kernel_decl.hpp"
