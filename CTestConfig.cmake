5b66d6d031abfda4f9692d5319e7806f3e5c762b
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2017-07-29 03:03:15 +0100

    Fix cdash upload.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -11,5 +11,5 @@ set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "my.cdash.org")
-set(CTEST_DROP_LOCATION "/submit.php?project=AFIO")
+set(CTEST_DROP_LOCATION "/submit.php?project=Boost.AFIO")
 set(CTEST_DROP_SITE_CDASH TRUE)
