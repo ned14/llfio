b911c790bc16ca29c3da1171902ea3fcc545f77d
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2017-08-06 02:14:16 +0100

    Fix clang build failure

--- include/afio/revision.hpp
+++ include/afio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define AFIO_PREVIOUS_COMMIT_REF    709130ce1ff4294a4527edeeeb73ec92e83fbc56
-#define AFIO_PREVIOUS_COMMIT_DATE   "2017-08-05 23:31:20 +00:00"
-#define AFIO_PREVIOUS_COMMIT_UNIQUE 709130ce
+#define AFIO_PREVIOUS_COMMIT_REF    3cbada12a0ba7add5d33c92cc62766a18e4701a2
+#define AFIO_PREVIOUS_COMMIT_DATE   "2017-08-06 00:24:11 +00:00"
+#define AFIO_PREVIOUS_COMMIT_UNIQUE 3cbada12
