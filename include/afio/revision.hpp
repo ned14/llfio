1b1526a91023aa170f7aadb834b9ff91d7136e96
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2017-08-17 01:17:50 +0100

    Fix programs build when in non-sibling config, and have Travis test those are building from now on.

--- include/afio/revision.hpp
+++ include/afio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define AFIO_PREVIOUS_COMMIT_REF    0c8f416d674e27ab2edfe1f1d045d4646abf7d78
-#define AFIO_PREVIOUS_COMMIT_DATE   "2017-08-16 22:08:53 +00:00"
-#define AFIO_PREVIOUS_COMMIT_UNIQUE 0c8f416d
+#define AFIO_PREVIOUS_COMMIT_REF    0bb001d4c854e36739b3b9cddbf183edc5982632
+#define AFIO_PREVIOUS_COMMIT_DATE   "2017-08-16 23:58:07 +00:00"
+#define AFIO_PREVIOUS_COMMIT_UNIQUE 0bb001d4
