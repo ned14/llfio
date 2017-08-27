a1addadaddfbe341364d7d507c6934800071d6d4
Jenkins nedprod CI <foo@nowhere>
2017-08-27 00:27:12 +0000

    Travis CI updates documentation

--- search/all_7.js
+++ search/all_7.js
@@ -5,6 +5,5 @@ var searchData=
   ['handle',['handle',['../classafio__v2__xxx_1_1handle.html',1,'afio_v2_xxx']]],
   ['handle_2ehpp',['handle.hpp',['../handle_8hpp.html',1,'']]],
   ['handle_5ftype',['handle_type',['../structafio__v2__xxx_1_1storage__profile_1_1item__base.html#a869a9463f4b6a014aa717040b1fc79f9',1,'afio_v2_xxx::storage_profile::item_base::handle_type()'],['../structafio__v2__xxx_1_1storage__profile_1_1storage__profile.html#afafa9f60b104d440106de2e0477316b1',1,'afio_v2_xxx::storage_profile::storage_profile::handle_type()']]],
-  ['hasher_5ftype',['hasher_type',['../classafio__v2__xxx_1_1algorithm_1_1shared__fs__mutex_1_1memory__map.html#a1acefdfab9ed10056232777b82c7725d',1,'afio_v2_xxx::algorithm::shared_fs_mutex::memory_map']]],
-  ['hook_5fresult_5fconstruction',['hook_result_construction',['../namespaceafio__v2__xxx.html#a6e724f1ed8bc70cb89ea1ef13176a4aa',1,'afio_v2_xxx']]]
+  ['hasher_5ftype',['hasher_type',['../classafio__v2__xxx_1_1algorithm_1_1shared__fs__mutex_1_1memory__map.html#a1acefdfab9ed10056232777b82c7725d',1,'afio_v2_xxx::algorithm::shared_fs_mutex::memory_map']]]
 ];
