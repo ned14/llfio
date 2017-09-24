2bb55316ae8fd3ea9b02679ece19a620447f3482
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2017-09-24 04:43:55 +0100

    Appveyor try VS2017 Preview

--- include/afio/revision.hpp
+++ include/afio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define AFIO_PREVIOUS_COMMIT_REF    d65a2c94daf7d2811d5bc22b9a6352b7012f3aae
-#define AFIO_PREVIOUS_COMMIT_DATE   "2017-09-23 23:10:12 +00:00"
-#define AFIO_PREVIOUS_COMMIT_UNIQUE d65a2c94
+#define AFIO_PREVIOUS_COMMIT_REF    19d2c9a62b802bf661acab09455f0427e05cc368
+#define AFIO_PREVIOUS_COMMIT_DATE   "2017-09-24 03:18:41 +00:00"
+#define AFIO_PREVIOUS_COMMIT_UNIQUE 19d2c9a6
