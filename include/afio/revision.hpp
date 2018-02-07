3170c9094d26cf35a858f3f48d040d9bc232f48c
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com <spamtrap@nedprod.com>
2018-02-07 09:03:20 +0000

    upd

--- include/afio/revision.hpp
+++ include/afio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define AFIO_PREVIOUS_COMMIT_REF    b957b73ca4eca7b484ef3e58e4dc8dbce43bc3ae
-#define AFIO_PREVIOUS_COMMIT_DATE   "2018-02-02 16:50:59 +00:00"
-#define AFIO_PREVIOUS_COMMIT_UNIQUE b957b73c
+#define AFIO_PREVIOUS_COMMIT_REF    808fda47f1194ad67f030b7510a998f77a568f0f
+#define AFIO_PREVIOUS_COMMIT_DATE   "2018-02-07 08:07:24 +00:00"
+#define AFIO_PREVIOUS_COMMIT_UNIQUE 808fda47
