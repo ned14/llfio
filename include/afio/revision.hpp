12e65bdbd981ac24b91af035cf0e1d73a1b4c4cb
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2018-04-17 13:12:15 +0100

    Added example use cases for kernel page allocation, and for shared memory allocation.

--- include/afio/revision.hpp
+++ include/afio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define AFIO_PREVIOUS_COMMIT_REF    3c096cba2f09986162490ea4cd0ce683af70cebd
-#define AFIO_PREVIOUS_COMMIT_DATE   "2018-04-15 12:18:15 +00:00"
-#define AFIO_PREVIOUS_COMMIT_UNIQUE 3c096cba
+#define AFIO_PREVIOUS_COMMIT_REF    8d96200572d8224dd460afe8cc3b7fd55227fa9c
+#define AFIO_PREVIOUS_COMMIT_DATE   "2018-04-16 15:39:11 +00:00"
+#define AFIO_PREVIOUS_COMMIT_UNIQUE 8d962005
