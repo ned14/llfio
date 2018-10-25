16c49eeb473fca306a00cc057a62745e96203f91
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2018-10-25 22:01:31 +0100

    upd

--- include/llfio/revision.hpp
+++ include/llfio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define LLFIO_PREVIOUS_COMMIT_REF    d5aa6ed95238f51a9184308f55b552fba836b50e
-#define LLFIO_PREVIOUS_COMMIT_DATE   "2018-10-19 15:59:03 +00:00"
-#define LLFIO_PREVIOUS_COMMIT_UNIQUE d5aa6ed9
+#define LLFIO_PREVIOUS_COMMIT_REF    a9bc36dd95c814bdcbd4eb02f45f1199839f12c8
+#define LLFIO_PREVIOUS_COMMIT_DATE   "2018-10-25 20:53:36 +00:00"
+#define LLFIO_PREVIOUS_COMMIT_UNIQUE a9bc36dd
