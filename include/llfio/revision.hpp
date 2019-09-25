7b5a2b5fa56cd53959e573a4750e09fe76030284
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2019-09-25 18:14:32 +0100

    Improve detection of coroutine support when on clang 9.

--- include/llfio/revision.hpp
+++ include/llfio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define LLFIO_PREVIOUS_COMMIT_REF    a3942420524c91179009e06b9d77cfb4326dcf7f
-#define LLFIO_PREVIOUS_COMMIT_DATE   "2019-09-19 09:51:59 +00:00"
-#define LLFIO_PREVIOUS_COMMIT_UNIQUE a3942420
+#define LLFIO_PREVIOUS_COMMIT_REF    aea9a0bda5ffc7aac4ba5e8c6582893b7aad7d90
+#define LLFIO_PREVIOUS_COMMIT_DATE   "2019-09-25 15:31:53 +00:00"
+#define LLFIO_PREVIOUS_COMMIT_UNIQUE aea9a0bd
