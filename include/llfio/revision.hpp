4bf1142ca61104193d061f58475b0f991de16398
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2019-10-04 14:24:28 +0100

    Replace git submodule based dependencies with cmake install based dependencies instead.

--- include/llfio/revision.hpp
+++ include/llfio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define LLFIO_PREVIOUS_COMMIT_REF    aea9a0bda5ffc7aac4ba5e8c6582893b7aad7d90
-#define LLFIO_PREVIOUS_COMMIT_DATE   "2019-09-25 15:31:53 +00:00"
-#define LLFIO_PREVIOUS_COMMIT_UNIQUE aea9a0bd
+#define LLFIO_PREVIOUS_COMMIT_REF    7b5a2b5fa56cd53959e573a4750e09fe76030284
+#define LLFIO_PREVIOUS_COMMIT_DATE   "2019-09-25 17:14:32 +00:00"
+#define LLFIO_PREVIOUS_COMMIT_UNIQUE 7b5a2b5f
