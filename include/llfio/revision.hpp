ca832a5ab9c05c82659b3ca32c16367da7694034
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2020-03-11 10:07:08 +0000

    Add Installability CI test.

--- include/llfio/revision.hpp
+++ include/llfio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define LLFIO_PREVIOUS_COMMIT_REF    1d97a5de10426b445a7ace2ea0e039c8a6c06ee3
-#define LLFIO_PREVIOUS_COMMIT_DATE   "2020-02-25 10:42:28 +00:00"
-#define LLFIO_PREVIOUS_COMMIT_UNIQUE 1d97a5de
+#define LLFIO_PREVIOUS_COMMIT_REF    6a1741fb75cb38d26933e8ee0bfa1eb6bb14f561
+#define LLFIO_PREVIOUS_COMMIT_DATE   "2020-02-26 11:00:03 +00:00"
+#define LLFIO_PREVIOUS_COMMIT_UNIQUE 6a1741fb
