c0f122ab7e1af69c4594608dd3399aa4711ee418
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2020-03-11 11:30:54 +0000

    Update cmake bootstrap script.

--- include/llfio/revision.hpp
+++ include/llfio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define LLFIO_PREVIOUS_COMMIT_REF    6a1741fb75cb38d26933e8ee0bfa1eb6bb14f561
-#define LLFIO_PREVIOUS_COMMIT_DATE   "2020-02-26 11:00:03 +00:00"
-#define LLFIO_PREVIOUS_COMMIT_UNIQUE 6a1741fb
+#define LLFIO_PREVIOUS_COMMIT_REF    60868da4b2ac32cbebdd122b531bf0ad8167c3a8
+#define LLFIO_PREVIOUS_COMMIT_DATE   "2020-03-11 10:36:10 +00:00"
+#define LLFIO_PREVIOUS_COMMIT_UNIQUE 60868da4
