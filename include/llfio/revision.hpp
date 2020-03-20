fa0a14d203c2673a814a77209dae8c045c68c193
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2020-03-20 10:17:51 +0000

    Improve probing of which <filesystem> to use.

--- include/llfio/revision.hpp
+++ include/llfio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define LLFIO_PREVIOUS_COMMIT_REF    a3dfa0db9326eab1f716d36679baeed52d9f40fb
-#define LLFIO_PREVIOUS_COMMIT_DATE   "2020-03-18 19:57:03 +00:00"
-#define LLFIO_PREVIOUS_COMMIT_UNIQUE a3dfa0db
+#define LLFIO_PREVIOUS_COMMIT_REF    8e02b46999597bb9eb414b09def487a599c28c0f
+#define LLFIO_PREVIOUS_COMMIT_DATE   "2020-03-19 09:14:59 +00:00"
+#define LLFIO_PREVIOUS_COMMIT_UNIQUE 8e02b469
