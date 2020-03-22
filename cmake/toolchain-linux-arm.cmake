5b60a4311ed5fadf6eba47cbf63dff5501c99b58
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2020-03-22 02:12:39 +0000

    travis

---
+++ cmake/toolchain-linux-arm.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR armhf)
+set(CMAKE_C_COMPILER arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER arm-linux-gnueabihf-g++)
+set(CMAKE_CXX_FLAGS "-Wno-attributes -Wno-psabi")
