c962f9acc4fa3732b2f646fd0d9041a4c3cc36a3
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2020-06-22 15:11:21 +0100

    Disable the status code builds entirely on Mac OS.

--- .ci.cmake
+++ .ci.cmake
@@ -100,7 +100,7 @@ else()
 endif()
 set(retval2 0)
 set(retval3 0)
-if(NOT CTEST_DISABLE_TESTING AND NOT APPLE)  # Recent XCode has a parse bug for asan and ubsan
+if(NOT CTEST_DISABLE_TESTING)
   if(("$ENV{CXX}" MATCHES "clang") OR ("$ENV{CXX}" MATCHES "g\\+\\+"))
     if("$ENV{CXX}" MATCHES "clang")
       ctest_build(TARGET _sl-asan)
