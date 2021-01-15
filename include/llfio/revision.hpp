11309114f6796db61a1c09c6e6848800f0e560d2
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2021-01-15 11:26:12 +0000

    Make the dynamic thread pool work scheduling not quite as strict in the native Linux implementation, now we only examine higher priority groups once per group cycle. This produces behaviours much more consistent with the Win32 thread pool and Grand Central Dispatch.

--- include/llfio/revision.hpp
+++ include/llfio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define LLFIO_PREVIOUS_COMMIT_REF    45112b3cffebb5f8409c0edfc8c8879a0aeaf516
-#define LLFIO_PREVIOUS_COMMIT_DATE   "2021-01-02 17:33:42 +00:00"
-#define LLFIO_PREVIOUS_COMMIT_UNIQUE 45112b3c
+#define LLFIO_PREVIOUS_COMMIT_REF    df3f68c97a25654d5fc659ada8a7bc04c7c80e84
+#define LLFIO_PREVIOUS_COMMIT_DATE   "2021-01-13 12:32:47 +00:00"
+#define LLFIO_PREVIOUS_COMMIT_UNIQUE df3f68c9
