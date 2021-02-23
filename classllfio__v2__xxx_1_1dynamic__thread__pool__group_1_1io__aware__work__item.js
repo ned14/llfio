a65090844aa1a2c4f93052a3700d8c138e165b84
ned14 <ned14@users.noreply.github.com>
2021-02-23 13:00:49 +0000

    Deploying to gh-pages from  @ d3562e013b92e59e332b382f68576c354dcff0f4 🚀

---
+++ classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.js
@@ -0,0 +1,19 @@
+var classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item =
+[
+    [ "io_handle_awareness", "structllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item_1_1io__handle__awareness.html", "structllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item_1_1io__handle__awareness" ],
+    [ "io_aware_work_item", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#aa2b895fcc057bd6ee33ba2c116e5e978", null ],
+    [ "io_aware_work_item", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#a9df0ab84dd9f69b40b9117a79c8eca78", null ],
+    [ "io_aware_work_item", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#aa090c92e4adf339d8092465934b82f03", null ],
+    [ "~io_aware_work_item", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#a08a869b40d064969f1b5f3536e2857a4", null ],
+    [ "_has_timer_set", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#a2c49153ff193f7b3d948eec37b5509f7", null ],
+    [ "_has_timer_set_absolute", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#a1c2850bf3778d32c335eace1f05ea3b7", null ],
+    [ "_has_timer_set_relative", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#aa6641deb837cc77540b1a9bdf41afa6f", null ],
+    [ "group_complete", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#ae8d6ba54f52e216edd37474f19731fd8", null ],
+    [ "handles", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#a0d45637f8c603e9f132f99d7c539ee22", null ],
+    [ "io_aware_next", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#ab92cec481e76ed36075100c0bf9f8821", null ],
+    [ "operator()", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#a68e317002adf480e0a4c076410e35e8e", null ],
+    [ "parent", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#aee890b7a94a8d7a24e9b55aef8177c49", null ],
+    [ "max_iosbusytime", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#a5860206701c43b7035475dc3f62a23ef", null ],
+    [ "max_iosinprogress", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#a240b9e4d3d0ee510c46347e735185688", null ],
+    [ "min_iosinprogress", "classllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item.html#a81540923522973756560e20c44a0e948", null ]
+];
--- search/variables_1.js
+++ search/variables_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['barriers',['barriers',['../structllfio__v2__xxx_1_1dynamic__thread__pool__group_1_1io__aware__work__item_1_1io__handle__awareness.html#a7def03d821a4b622630749cc95d66768',1,'llfio_v2_xxx::dynamic_thread_pool_group::io_aware_work_item::io_handle_awareness']]],
   ['base',['base',['../structllfio__v2__xxx_1_1io__multiplexer_1_1__unsynchronised__io__operation__state_1_1payload__t_1_1noncompleted__t.html#ab829e3c5a1116cb43588e4d70d94396c',1,'llfio_v2_xxx::io_multiplexer::_unsynchronised_io_operation_state::payload_t::noncompleted_t']]]
 ];
