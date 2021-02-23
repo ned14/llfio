94534ac77630169323832b47fa96cba91113cf2b
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2021-02-23 21:27:34 +0000

    Implement dynamic_thread_pool_group benchmark.

--- include/llfio/revision.hpp
+++ include/llfio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define LLFIO_PREVIOUS_COMMIT_REF    c9e8352c9314ef2f2aa58fd9c15cdc36da9c99ac
-#define LLFIO_PREVIOUS_COMMIT_DATE   "2021-02-17 09:27:27 +00:00"
-#define LLFIO_PREVIOUS_COMMIT_UNIQUE c9e8352c
+#define LLFIO_PREVIOUS_COMMIT_REF    1cf5addd4a11570624f8faf17491431d7dde5427
+#define LLFIO_PREVIOUS_COMMIT_DATE   "2021-02-23 12:58:16 +00:00"
+#define LLFIO_PREVIOUS_COMMIT_UNIQUE 1cf5addd
