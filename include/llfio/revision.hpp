6eaa28369ad194f18c072670121aa61d9f6a48a6
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2023-07-20 10:15:52 +0100

    Respect BUILD_TESTING and BUILD_SHARED_LIBS (if testing is disabled).

--- include/llfio/revision.hpp
+++ include/llfio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define LLFIO_PREVIOUS_COMMIT_REF    ee65f1b99ecdf12ee0d011e3d8e3b1c6eae67644
-#define LLFIO_PREVIOUS_COMMIT_DATE   "2023-06-20 11:47:43 +00:00"
-#define LLFIO_PREVIOUS_COMMIT_UNIQUE ee65f1b9
+#define LLFIO_PREVIOUS_COMMIT_REF    583f838bf7fa5a18a521ad040ffafbb574383b52
+#define LLFIO_PREVIOUS_COMMIT_DATE   "2023-06-28 12:06:41 +00:00"
+#define LLFIO_PREVIOUS_COMMIT_UNIQUE 583f838b
