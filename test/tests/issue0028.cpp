4fc8c46b416900a01ebe5ca5ee64e530392f792a
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2024-08-22 11:52:05 +0100

    Fix issue0028 making assumptions that the page size is 4Kb

--- test/tests/issue0028.cpp
+++ test/tests/issue0028.cpp
@@ -33,37 +33,37 @@ static inline void TestIssue28()
     BOOST_CHECK_EQUAL(oh.map().capacity(), 0);
     oh.truncate(4100).value();
     BOOST_CHECK_EQUAL(oh.capacity(), 4200);
-    BOOST_CHECK_EQUAL(oh.map().capacity(), 8192);
+    BOOST_CHECK_EQUAL(oh.map().capacity(), llfio::utils::round_up_to_page_size(4200u, oh.page_size()));
     BOOST_CHECK_EQUAL(oh.map().length(), 4100);
     BOOST_CHECK_EQUAL(oh.maximum_extent().value(), 4100);  // calls update_map()
     BOOST_CHECK_EQUAL(oh.capacity(), 4200);
-    BOOST_CHECK_EQUAL(oh.map().capacity(), 8192);
+    BOOST_CHECK_EQUAL(oh.map().capacity(), llfio::utils::round_up_to_page_size(4200u, oh.page_size()));
     BOOST_CHECK_EQUAL(oh.map().length(), 4100);
 
     auto ih = llfio::mapped_file(8100, {}, oh.current_path().value(), llfio::mapped_file_handle::mode::write).value();
     BOOST_CHECK_EQUAL(ih.capacity(), 8100);
-    BOOST_CHECK_EQUAL(ih.map().capacity(), 8192);
+    BOOST_CHECK_EQUAL(ih.map().capacity(), llfio::utils::round_up_to_page_size(8100u, ih.page_size()));
     BOOST_CHECK_EQUAL(ih.map().length(), 4100);
     BOOST_CHECK_EQUAL(ih.maximum_extent().value(), 4100);  // calls update_map()
     BOOST_CHECK_EQUAL(ih.capacity(), 8100);
-    BOOST_CHECK_EQUAL(ih.map().capacity(), 8192);
+    BOOST_CHECK_EQUAL(ih.map().capacity(), llfio::utils::round_up_to_page_size(8100u, ih.page_size()));
     BOOST_CHECK_EQUAL(ih.map().length(), 4100);
 
     oh.truncate(12100).value();
     BOOST_CHECK_EQUAL(oh.capacity(), 12100);
-    BOOST_CHECK_EQUAL(oh.map().capacity(), 12288);
+    BOOST_CHECK_EQUAL(oh.map().capacity(), llfio::utils::round_up_to_page_size(12100u, oh.page_size()));
     BOOST_CHECK_EQUAL(oh.map().length(), 12100);
     BOOST_CHECK_EQUAL(oh.maximum_extent().value(), 12100);  // calls update_map()
     BOOST_CHECK_EQUAL(oh.capacity(), 12100);
-    BOOST_CHECK_EQUAL(oh.map().capacity(), 12288);
+    BOOST_CHECK_EQUAL(oh.map().capacity(), llfio::utils::round_up_to_page_size(12100u, oh.page_size()));
     BOOST_CHECK_EQUAL(oh.map().length(), 12100);
 
     BOOST_CHECK_EQUAL(ih.capacity(), 8100);
-    BOOST_CHECK_EQUAL(ih.map().capacity(), 8192);
+    BOOST_CHECK_EQUAL(ih.map().capacity(), llfio::utils::round_up_to_page_size(8100u, ih.page_size()));
     BOOST_CHECK_EQUAL(ih.map().length(), 4100);
     BOOST_CHECK_EQUAL(ih.maximum_extent().value(), 8100);  // must not exceed reservation
     BOOST_CHECK_EQUAL(ih.capacity(), 8100);
-    BOOST_CHECK_EQUAL(ih.map().capacity(), 8192);
+    BOOST_CHECK_EQUAL(ih.map().capacity(), llfio::utils::round_up_to_page_size(8100u, ih.page_size()));
     BOOST_CHECK_EQUAL(ih.map().length(), 8100);
   }  // namespace llfio=LLFIO_V2_NAMESPACE;
   {
@@ -75,8 +75,12 @@ static inline void TestIssue28()
   }  // namespace llfio=LLFIO_V2_NAMESPACE;
   {
     // This line will fail to set file to sparse due to Access Denied, caused by using a read-only mode to create a file :)
-    auto oh = llfio::mapped_temp_file(8100, {}, llfio::mapped_file_handle::mode::read, llfio::mapped_file_handle::creation::if_needed, llfio::mapped_file_handle::caching::temporary, llfio::mapped_file_handle::flag::none).value();
-    auto ih = llfio::mapped_file(8100, {}, oh.current_path().value(), llfio::mapped_file_handle::mode::write, llfio::mapped_file_handle::creation::if_needed, llfio::mapped_file_handle::caching::temporary, llfio::mapped_file_handle::flag::unlink_on_first_close).value();
+    auto oh = llfio::mapped_temp_file(8100, {}, llfio::mapped_file_handle::mode::read, llfio::mapped_file_handle::creation::if_needed,
+                                      llfio::mapped_file_handle::caching::temporary, llfio::mapped_file_handle::flag::none)
+              .value();
+    auto ih = llfio::mapped_file(8100, {}, oh.current_path().value(), llfio::mapped_file_handle::mode::write, llfio::mapped_file_handle::creation::if_needed,
+                                 llfio::mapped_file_handle::caching::temporary, llfio::mapped_file_handle::flag::unlink_on_first_close)
+              .value();
     ih.truncate(6100).value();
     BOOST_CHECK_EQUAL(oh.maximum_extent().value(), 6100);
     BOOST_CHECK_EQUAL(ih.maximum_extent().value(), 6100);
@@ -88,7 +92,7 @@ static inline void TestIssue28()
     auto ih = llfio::mapped_file(8100, {}, oh.current_path().value()).value();
     oh.truncate(12100).value();
     BOOST_CHECK_EQUAL(ih.maximum_extent().value(), 8100);  // must not exceed reservation
-  }                                                        // namespace llfio=LLFIO_V2_NAMESPACE;
+  }  // namespace llfio=LLFIO_V2_NAMESPACE;
   {
     auto oh = llfio::mapped_temp_file(4200).value();
     auto ih = llfio::mapped_file(8100, {}, oh.current_path().value()).value();
