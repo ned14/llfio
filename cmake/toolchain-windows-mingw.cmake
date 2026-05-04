6a2e4dd27c7b5481f12bb5d0ec19887d219d8f78
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2026-05-04 21:33:03 +0100

    wip

--- cmake/toolchain-windows-mingw.cmake
+++ cmake/toolchain-windows-mingw.cmake
@@ -2,7 +2,7 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
-## mingw doesn't support __try ... __except
+## mingw doesn't support __try ... __except, so disable signal guard
 set(CMAKE_CXX_FLAGS_INIT "-DLLFIO_DISABLE_SIGNAL_GUARD=1 -Wno-cast-function-type -Wno-maybe-uninitialized -Wno-unused-local-typedefs -Wno-unknown-pragmas -Wno-c++20-compat")
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
