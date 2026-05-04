f3355a5eb13d62ed6149b1f08a70516bd760dff2
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nedprod.com>
2026-05-04 01:26:50 +0100

    Work around Mingw path drive letter munging.

--- include/llfio/revision.hpp
+++ include/llfio/revision.hpp
@@ -1,4 +1,4 @@
 // Note the second line of this file must ALWAYS be the git SHA, third line ALWAYS the git SHA update time
-#define LLFIO_PREVIOUS_COMMIT_REF    f386fd6bb9fb3dfa56612f9732479e4eb0a65527
-#define LLFIO_PREVIOUS_COMMIT_DATE   "2026-03-10 20:01:08 +00:00"
-#define LLFIO_PREVIOUS_COMMIT_UNIQUE f386fd6b
+#define LLFIO_PREVIOUS_COMMIT_REF    5d34a2afb8ab16a6cb5fb279a897f95cf3713a59
+#define LLFIO_PREVIOUS_COMMIT_DATE   "2026-05-03 23:43:23 +00:00"
+#define LLFIO_PREVIOUS_COMMIT_UNIQUE 5d34a2af
